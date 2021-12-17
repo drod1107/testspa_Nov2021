41e5a3fcb0454cf8527029c0c79040400f73967f
David Rodriguez <davidrodrigueznolimit@gmail.com>
2021-12-16 19:07:04 -0600

    Initial commit

--- index.js
+++ index.js
@@ -5,15 +5,27 @@ import { capitalize } from "lodash";
 
 const router = new Navigo(window.location.origin);
 
-router
-  .on({
-    "/": () => render(state.Home),
-    ":page": params => {
-      let page = capitalize(params.page);
-      render(state[page]);
+router.hooks({
+  before: (done, params) => {
+    const page = params && params.hasOwnProperty("page") ? capitalize(params.page) : "Home";
+
+    if (page === "Home") {
+      axios
+        .get(
+          `https://api.openweathermap.org/data/2.5/weather?appid=fbb30b5d6cf8e164ed522e5082b49064&q=st.%20louis`
+        )
+        .then(response => {
+          state.Home.weather = {};
+          state.Home.weather.city = response.data.name;
+          state.Home.weather.temp = response.data.main.temp;
+          state.Home.weather.feelsLike = response.data.main.feels_like;
+          state.Home.weather.description = response.data.weather[0].main;
+          done();
+        })
+        .catch(err => console.log(err));
     }
-  })
-  .resolve();
+  }
+});
 
 function render(st) {
   document.querySelector("#root").innerHTML = `
